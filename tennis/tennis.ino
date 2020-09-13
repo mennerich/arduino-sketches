2a37ae3289adaad104a4ed421f4ede7682fe9c0f
Donald Mennerich <don.mennerich@nyu.edu>
2020-09-13 13:04:47 -0400

    updating tennis

--- tennis/tennis.ino
+++ tennis/tennis.ino
@@ -1,114 +1,124 @@
-int ledArray[] = {8,9,10,11,12};
-int count = 0;
-int r = 0;
-int t = 75;
-int counter = 0;
-int upPlayer = 8;
-int downPlayer = 12;
-int upScore = 0;
-int downScore = 0;
+const int ledArray[] = {8,9,10,11,12};
+const int upPlayer = 8;
+int upScore, downScore = 0;
+const int downPlayer = 12;
+const int defaultTimer = 75;
+int currentServe;
 
 void setup() {
   Serial.begin(9600);
   randomSeed(analogRead(0));
-  for(count=0; count < 5; ++count) {
-    pinMode(ledArray[count], OUTPUT);
+  for(int i= 0; i < 5; ++i) {
+    pinMode(ledArray[i], OUTPUT);
   }
-  
-  opening();
-  Serial.println("BEGIN!");
-}
 
-void opening() {
-  for(count=0; count < 5; ++count) {
-    digitalWrite(downPlayer, HIGH);
-    digitalWrite(upPlayer, HIGH);
-    delay(t);
-    digitalWrite(downPlayer, LOW);
-    digitalWrite(upPlayer, LOW);
-    delay(t);
-  }
+  Serial.println("Tennis, v0.1.0");
+  opening();
+  Serial.println("The Match Bengins!");
+  currentServe = coinToss();
 }
 
 void loop() {
-  if(counter == 5) {
-    t = random(10, 25);  
-    counter = 0;
+  volley(); 
+}
+
+void opening() {
+  for(int i = 0; i < 5; ++i) {
+    digitalWrite(downPlayer, HIGH);
+    digitalWrite(upPlayer, HIGH);
+    delay(defaultTimer);
+    digitalWrite(downPlayer, LOW);
+    digitalWrite(upPlayer, LOW);
+    delay(defaultTimer);
   }
-  
-  r = random(0,2);
-  if(r ==1) {
-    up(t);
+}
+
+int coinToss() {
+  int coin = random(0,2);
+  if(coin == 0) {
+    pulse(upPlayer, 5, defaultTimer);
+    return upPlayer;
+  } else if(coin == 1) {
+    pulse(downPlayer, 5, defaultTimer); 
+    return downPlayer;
+  }
+}
+
+void pulse(int player, int count, int timer) {
+  for(int i = 0; i < count; i++) {
+    digitalWrite(player, HIGH);
+    delay(defaultTimer);
+    digitalWrite(player, LOW);
+    delay(defaultTimer);
+  }
+}
+
+void checkForWin() {
+  if(upScore == 11) {
+    pulse(upPlayer, 10, defaultTimer);
+    endGame();
+  } else if (downScore == 1) {
+    pulse(downPlayer, 10, defaultTimer);
+    endGame();
+  }
+}
+
+void endGame() {
+  while(1);
+}
+
+void volley() {
+  checkForWin();
+  if(currentServe == upPlayer) {
+    upHit();
   } else {
-    down(t);
-  }
-
-  ++counter;
-}
-
-void up(int timer) {
-  for(count=0; count < 5; ++count) {
-    digitalWrite(ledArray[count], HIGH);
-    delay(timer);
-    digitalWrite(ledArray[count], LOW);
-    delay(timer);
-    score(upPlayer);
+    downHit();
   }
 
 }
 
-void down(int timer) {
-  for(count=4; count > -1; --count) {
-    digitalWrite(ledArray[count], HIGH);
-    delay(timer);
-    digitalWrite(ledArray[count], LOW);
-    delay(timer);
-    score(downPlayer);
-  }
 
-}
-
-void score(int i) {
-  int shot = random(0, 200);
-  
-  if(shot > 195) {
-    Serial.print("SCORE!");
-    
-    if(i == upPlayer) {
-      ++upScore;
-    } else if(i == downPlayer){
-      ++downScore;
-    }
-
-    if(upScore == 11) {
-      Serial.print("Up Wins!!");
-      win(upPlayer); 
-    } else if (downScore == 11) {
-      Serial.print("Down Wins!!");
-      win(downPlayer);
-    }
-
-    Serial.print("Up: ");
-    Serial.print(upScore);
-    Serial.print("\tDown: ");
-    Serial.print(downScore);
-    Serial.print("\n");
-    
-    for(int j = 0; j < 3; ++j) {
-      digitalWrite(i, HIGH);
-      delay(500);
-      digitalWrite(i, LOW);
-      delay(500); 
-    }
+bool winner() {
+  int r = random(0, 200);
+  if(r > 194) {
+    return true;
+  } else {
+    return false;
   }
 }
 
-void win(int winner) {
-    for(int j = 0; j < 10; ++j) {
-      digitalWrite(winner, HIGH);
-      delay(500);
-      digitalWrite(winner, LOW);
-      delay(500); 
-    }
-     while(1);
+void upHit() {
+  int timer = random(0,75);
+  for(int i = 0; i < 4; ++i) {
+    digitalWrite(ledArray[i], HIGH);
+    delay(timer);
+    digitalWrite(ledArray[i], LOW);
+    delay(timer);   
+  }
+  if(winner){
+    upScore++;
+    pulse(upPlayer, 4, defaultTimer);
+    currentServe = upPlayer;
+    volley();
+  } else {
+    downHit();
+  }
+}
+
+void downHit() {
+  int timer = random(0,75);
+  for(int i = 4; i > 0; --i) {
+    digitalWrite(ledArray[i], HIGH);
+    delay(timer);
+    digitalWrite(ledArray[i], LOW);
+    delay(timer); 
+  }
+  if(winner){
+    downScore++;
+    pulse(downPlayer, 4, defaultTimer);
+    currentServe = downPlayer;
+    volley();
+  } else {
+    upHit();
+  }
 }
